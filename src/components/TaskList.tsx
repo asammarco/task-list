79f901d1641146a4fd6254423fc909647c2bcb9d
asammarco <asammarco@hotmail.com>
2021-03-12 09:42:04 -0300

    add Create, Remove and Toggle tasks

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,28 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+     newTaskTitle.trim() && setTasks([...tasks, {
+      id: Math.floor(Math.random()*999999),
+      title: newTaskTitle,
+      isComplete: false,
+    }]);
+    setNewTaskTitle('');
   }
 
-  function handleToggleTaskCompletion(id: number) {
+  function handleToggleTaskCompletion(id: number){
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(tasks.map(task => {
+      if(task.id === id){
+          task.isComplete = !task.isComplete
+        }
+      return task;
+    }));
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const newTasks = tasks.filter(task => task.id !== id);
+    setTasks(newTasks);
   }
 
   return (
